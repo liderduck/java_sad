07d93d3d6c295d791d535e15312cf3ac28c10bbe
anahe <jonathanguijarro@gmail.com>
2016-02-06 14:42:29 +0100

    terminado

--- Sad/src/sad/Seleccion.java
+++ Sad/src/sad/Seleccion.java
@@ -1,15 +1,7 @@
 package sad;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Random;
 import weka.attributeSelection.BestFirst;
 import weka.attributeSelection.CfsSubsetEval;
-import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.lazy.IB1;
-import weka.core.Capabilities;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
