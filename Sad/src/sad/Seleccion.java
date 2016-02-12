95954269c638d1e5afd3ce93e548f6a584b78f99
jonathan <jonathanguijarro@gmail.com>
2016-02-12 18:30:21 +0100

    comentarios

--- Sad/src/sad/Seleccion.java
+++ Sad/src/sad/Seleccion.java
@@ -52,8 +52,8 @@ public class Seleccion {
 		estimador.setKNN(1);//esto sirve para añadir los vecinos al ibk
 		evaluator = evalKFold(dataSel, estimador);
 		mejorFM=evaluator.fMeasure(0);
-		for(int k=2;k<=30;k++){
-			estimador.setKNN(k);//esto sirve para añadir los vecinos al ibk
+		for(int k=2;k<=30;k++){//aumenta los vecinos y los va probando
+			estimador.setKNN(k);
 			evaluator = evalKFold(dataSel, estimador);
 			actualFM=evaluator.fMeasure(0);
 			if (actualFM>mejorFM){
