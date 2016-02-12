595ab2ea58e1cb32668b85b0078d6103445fa80b
jonathan <jonathanguijarro@gmail.com>
2016-02-12 17:50:47 +0100

    mejoras de modularidad/rendimiento

--- Sad/src/sad/Escribir.java
+++ Sad/src/sad/Escribir.java
@@ -3,17 +3,21 @@ package sad;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
 import weka.core.Instances;
 
 
 public class Escribir {
 	
 	public void escribir(double[] predictions,Instances inst){
+
 		if (inst==null){
 			FileWriter fichero = null;
 			PrintWriter pw = null;
 			try{
 				fichero=new FileWriter ("C:\\Users\\jonathan\\Downloads\\clases.txt");
+				pw= new PrintWriter(fichero);
 				for (int z=0;z< predictions.length;z++){
 					pw.print(predictions[z]);
 					pw.println();
@@ -34,7 +38,7 @@ public class Escribir {
 			PrintWriter pw = null;
 			try{
 				int z=0;
-				fichero=new FileWriter ("C:\\Users\\jonathan\\Downloads\\clases1.txt");
+				fichero=new FileWriter ("C:\\Users\\jonathan\\Downloads\\test.txt");
 				pw= new PrintWriter(fichero);
 				int ultimo =  inst.numInstances();
 				
@@ -57,5 +61,15 @@ public class Escribir {
 			}
 			
 		}
+	}
+	
+	public void escClases(Instances test,Evaluation evaluator2,Classifier estimador) throws Exception{
+		double predictions[] = new double[test.numInstances()];
+		for (int i = 0; i < test.numInstances(); i++) {
+			predictions[i] = evaluator2.evaluateModelOnceAndRecordPrediction(estimador, test.instance(i));
+		}
+		/////////////Guardar en un fichero de salida la clase estimada por el modelo para cada instancia del test y así después podremos comparar la clase real y la estimada
+		
+		escribir(predictions,null);
 	}
 }
